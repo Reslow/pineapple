0e1c3597a09c466d7d46dee8b61032657237abf4
Fanny Reslow <fr222er@student.lnu.se>
2019-09-10 20:36:01 +0200

    Ã starting on aboutpage

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,15 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <div id="tWrap">
+    <p>/About<p/>
+    <div class="text">
+      <img src="../../static/profileround.png" alt="AR icon" style="width:10%">
+    
+    <p>As a UX designer I am always focused on the user, what the user wants and more important the users needs. I am a good listener and I think I am good at shifting perspectives when thinking of design, this give more depth to the thoughts behind the design. I know how to conduct user research and how to produce products depending on user research. In june 2019 I will take my bachelor in interaction design at Linneaus University. I have been working with the design process from idea to interactive prototypes in various projects during my education</p>
+</div>
+</div>
   </Layout>
+
 </template>
 
 <script>
@@ -12,3 +19,21 @@ export default {
   }
 }
 </script>
+<style>
+.navP{
+  color: gray;
+  padding:20px;
+}
+.text{
+  color: rgb(0, 0, 0);
+ 
+  
+}
+#twrap{
+  margin: flex;
+  display:flex;
+}
+
+
+
+</style>
